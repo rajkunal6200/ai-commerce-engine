70b27fa50bace64e3138965d8cfe5a8b1ff82643
Kunal Raj <kunalraj31012005@gmail.com>
2026-09-04 11:13:06 +0530

    chore: make CORS production configurable

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        os.getenv("FRONTEND_ORIGIN", ""),
     ],
     allow_credentials=True,
     allow_methods=["*"],
